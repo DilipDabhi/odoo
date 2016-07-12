502b4717ad94ffa5ff6b35f58960b691eb8b46f5
ddabhi <ddabhi@nishutatechnolabs.com>
2016-07-12 19:04:05 +0530

    Update module

---
+++ test_backend_theme/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Test Backend Themes',
+    'category': 'Website',
+    'summary': 'Website',
+    'website': '',
+    'version': '1.0',
+    'description': """
+OpenERP Website
+===================
+        """,
+    'depends': [],
+    'installable': True,
+    'data': [
+        'views/website_templates.xml',
+    ],
+    'application': True,
+}
